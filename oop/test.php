89a143d390ffc98ff302330ce38a6a6520205f40
martinostrat <martin.ostrat@khk.ee>
2017-01-12 11:33:09 +0200

    php class constructor

--- oop/test.php
+++ oop/test.php
@@ -16,4 +16,15 @@ echo '</pre>';
 
 $sentence1->show();
 echo '<hr/>';
+
+
+$sentence2 = new text('Tere koos konstruktoriga');
+
+echo '<pre>';
+print_r($sentence2);
+echo '</pre>';
+
+$sentence2->show();
+echo '<hr/>';
+
 ?>
