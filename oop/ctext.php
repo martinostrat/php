def496f822e607e86ee8ede3a60c64652dfe37c4
martinostrat <martin.ostrat@khk.ee>
2017-01-13 12:16:12 +0200

    set color variable

---
+++ oop/ctext.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: martinostrat
+ * Date: 1/13/17
+ * Time: 12:05 PM
+ */
+
+require_once 'text.php';
+
+class ctext extends text {
+
+    // class variable - color
+    var $color = false; // color doesnt exist
+
+    // methods
+    // set color
+    function setColor($c) {
+        $this->color = $c;
+    }
+
+
+}
